4943507024212e2d33af48c5d80aad29a9e92dc7
Ivan Zhang <ivanzhang410@gmail.com>
2019-06-25 11:19:01 -0400

    using RE in reversWords

--- code/String/reverseWords.js
+++ code/String/reverseWords.js
@@ -1,5 +1,16 @@
+
+//method using split API
+// export default (str) => {
+// 	return str.split(' ').map(item => {
+// 		return item.split('').reverse().join('');
+// 	}).join(' ');
+// }
+
+
+//using regex
 export default (str) => {
-	return str.split(' ').map(item => {
+	//regex match
+	return str.match(/[\w']+/g).map(item => {
 		return item.split('').reverse().join('');
 	}).join(' ');
 }
