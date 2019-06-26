ce79e0a78af164618c7a4350a4f09a457b7d9c94
Ivan Zhang <ivanzhang410@gmail.com>
2019-06-26 10:28:54 -0400

    add comments in 557

--- code/String/reverseWords.js
+++ code/String/reverseWords.js
@@ -1,3 +1,11 @@
+/**
+* LeetCode - 557
+* 557. Reverse Words in a String III
+*
+* Given a string, you need to reverse the order of characters
+* in each word within a sentence while still preserving whitespace and initial word order.
+*
+*/
 
 //method using split API
 // export default (str) => {
