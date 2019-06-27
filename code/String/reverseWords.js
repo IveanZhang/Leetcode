a85607d8de3faf1f5225903d7ac4c7d9d070e8bb
Ivan Zhang <ivanzhang410@gmail.com>
2019-06-27 17:58:24 -0400

    add 696 count binary substrings

--- code/String/reverseWords.js
+++ code/String/reverseWords.js
@@ -1,11 +1,11 @@
 /**
-* LeetCode - 557
-* 557. Reverse Words in a String III
-*
-* Given a string, you need to reverse the order of characters
-* in each word within a sentence while still preserving whitespace and initial word order.
-*
-*/
+ * LeetCode - 557
+ * 557. Reverse Words in a String III
+ *
+ * Given a string, you need to reverse the order of characters
+ * in each word within a sentence while still preserving whitespace and initial word order.
+ *
+ */
 
 //method using split API
 // export default (str) => {
@@ -17,8 +17,8 @@
 
 //using regex
 export default (str) => {
-	//regex match
-	return str.match(/[\w']+/g).map(item => {
-		return item.split('').reverse().join('');
-	}).join(' ');
+    //regex match
+    return str.match(/[\w']+/g).map(item => {
+        return item.split('').reverse().join('');
+    }).join(' ');
 }
