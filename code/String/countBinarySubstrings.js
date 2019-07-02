bea348b016953f4b9379679d43b2d3cd0d8a5de2
Ivan Zhang <ivanzhang410@gmail.com>
2019-07-02 11:08:29 -0400

    add inputs and outputs samples

--- code/String/countBinarySubstrings.js
+++ code/String/countBinarySubstrings.js
@@ -5,6 +5,12 @@
 * Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's, 
   and all the 0's and all the 1's in these substrings are grouped consecutively. 
   Substrings that occur multiple times are counted the number of times they occur.
+
+  Input: "00110011"
+  Output: 6
+
+  Input: "10101"
+  Output: 4
 *
 */
 
--- code/String/reverseWords.js
+++ code/String/reverseWords.js
@@ -5,6 +5,8 @@
  * Given a string, you need to reverse the order of characters
  * in each word within a sentence while still preserving whitespace and initial word order.
  *
+   Input: "Let's take LeetCode contest"
+   Output: "s'teL ekat edoCteeL tsetnoc"
  */
 
 //method using split API
